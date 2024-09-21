5bd89c2afd6b65442a09ee9ab90f46ab49b0771e
ksobrenat32 <ksobrenatural@tuta.io>
2024-09-20 22:14:30 -0600

    Funciones 1 y 2

--- backend/user.py
+++ backend/user.py
@@ -1,12 +1,14 @@
-def agregar_usuario(conn, nombre, email, contrasena) -> None:
+# Agregar un usuario a la base de datos
+def add_user(conn, name, email, password) -> None:
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO usuarios (nombre, contrasena) VALUES (?, ?)", (nombre, contrasena))
+    cursor.execute("INSERT INTO usuarios (nombre, correo, contrasena) VALUES (?, ?, ?)", (name, email, password))
     conn.commit()
 
-def comprobar_credenciales(conn, email, contrasena) -> bool:
+# Comprobar credenciales de un usuario
+def check_login(conn, email, password) -> bool:
     cursor = conn.cursor()
-    cursor.execute("SELECT contrasena FROM usuarios WHERE email = ?", (email,))
+    cursor.execute("SELECT contrasena FROM usuarios WHERE correo = ?", (email,))
     resultado = cursor.fetchone()
     if not resultado:
         return False
-    return resultado[0] == contrasena
+    return resultado[0] == password

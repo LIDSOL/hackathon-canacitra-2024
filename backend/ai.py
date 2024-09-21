b407c8216f3e4eeae51e3a60be28f7688d017549
ksobrenat32 <ksobrenatural@tuta.io>
2024-09-20 22:19:33 -0600

    Funcion ai_guess_delay

--- backend/ai.py
+++ backend/ai.py
@@ -1,15 +1,14 @@
-import os
-from openai import OpenAI
+from openai import ai
 
-client = OpenAI(
-    api = os.getenv("OPENAI_API_KEY")
-)
-
+# INPUT: "Natural language message of a subway delay"
+# OUTPUT: "JSON object with the subway lines and the estimated delay in minutes"
 def ai_guess_delay(message) -> str:
+    client = ai()
+
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "The following message is a tweet from a official subway account. Please, if the message is that a subway line or lines are delayed (now or in the future, ignore if it was in the past), return an array of lines and the estimated delay in minutes if no time is given try to guess a number on the range from 1 to 60 minutes, otherwise return an empty array and 0 minutes. The response should be in raw json format. There lines are 1-9, A,B and 12 The attributes are 'lines' and 'delay'."},
+            {"role": "system", "content": "The following message is a tweet from a official subway account. Please, if the message is that a subway line or lines are delayed (now or in the future, ignore if it was in the past), return an array of lines and the estimated delay in minutes if no time is given try to guess a number on the range from 1 to 60 minutes, otherwise return an empty array and 0 minutes. People who fall to the rails cause a delay of no less than 30 minutes. The response should be in raw json format, no markdown, only json. There lines are 1-9, A,B and 12 The attributes are 'lines' and 'delay'."},
             {
                 "role": "user",
                 "content": message

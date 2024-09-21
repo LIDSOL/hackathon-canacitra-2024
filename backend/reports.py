65403bee8fc8c024ac29e0a002e1f9bf73bc580a
ksobrenat32 <ksobrenatural@tuta.io>
2024-09-21 02:05:50 -0600

    Actualiza con los ultimos tweets

--- backend/reports.py
+++ backend/reports.py
@@ -1,5 +1,6 @@
 from ai import ai_guess_report
 from db import actualizar_linea
+from scrape import save_oficial_reports
 
 # Añadir un reporte de un usuario
 # INPUT: id_usuario, mensaje
@@ -52,6 +53,8 @@ def get_line_reports(conn, linea) -> int:
 def update_delays(conn) -> None:
     cursor = conn.cursor()
 
+    save_oficial_reports(conn)
+
     # Obtener todas las lineas
     cursor.execute("SELECT nombre FROM lineas")
     lineas = [linea[0] for linea in cursor.fetchall()]
@@ -60,9 +63,24 @@ def update_delays(conn) -> None:
         # Obtener el numero de reportes de la ultima hora
         reportes = get_line_reports(conn, linea)
 
+        # Si hay un reporte del usuario 0 en la última hora, su valor de la fecha significa a la hora que se debe terminar el retraso
+        cursor.execute("SELECT fecha FROM reportes_usuario WHERE usuario = 0 AND linea = ? AND fecha > datetime('now', '-1 hour')", (linea,))
+        fecha = cursor.fetchone()
+        if fecha:
+            cursor.execute("SELECT strftime('%s', ?) - strftime('%s', 'now')", (fecha[0],))
+            tiempo_restante = cursor.fetchone()[0]
+            if tiempo_restante <= 0:
+                actualizar_linea(conn, linea, 0)
+                continue
+
         # Si hay reportes, actualizar el tiempo de espera
         if reportes > 3:
             actualizar_linea(conn, linea, reportes)
         else:
             actualizar_linea(conn, linea, 0)
     conn.commit()
+
+# Demo
+#from db import conexion_base_de_datos
+#conn = conexion_base_de_datos()
+#update_delays(conn)
